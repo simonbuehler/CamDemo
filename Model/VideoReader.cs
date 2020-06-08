cc3a4e7f84fdca86c5c3fe33464389e4409a60f9
simon <78061+simonbuehler@users.noreply.github.com>
2020-06-08 11:30:47 +0200

    add

---
+++ Model/VideoReader.cs
@@ -0,0 +1,46 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CamDemo.model
+{
+    class VideoReader
+    {
+        private Mat frameMat;
+        private bool ended = false;
+        private readonly VideoCapture videoCapture;
+
+        public VideoReader()
+        {
+            videoCapture = new VideoCapture(0);
+        }
+
+        public async Task ReadFrame(IProgress<ProcessedFrame> progress, CancellationToken token)
+        {
+            await Task.Run(async () =>
+            {
+                 while (!(token.IsCancellationRequested || ended))
+                {
+                    using (frameMat = videoCapture.RetrieveMat())
+                    {
+                        ended = frameMat.Empty();
+                        var result = WriteableBitmapConverter.ToWriteableBitmap(frameMat);
+                        result.Freeze();
+                        progress.Report(new ProcessedFrame { Bitmap = result });
+                    }
+                }
+                await Task.Delay(5);
+                Console.WriteLine("done");
+            });
+        }
+    }
+
+    class ProcessedFrame
+    {
+        public WriteableBitmap Bitmap;
+        //more properties
+    }
+}
